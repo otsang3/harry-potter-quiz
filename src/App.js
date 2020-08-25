64e280298236f7ba3ffe1340764e19a5e2d90fb3
otsang3 <ohhtsang@gmail.com>
2020-08-25 14:44:03 +0100

    coded question.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Question from './components/Question';
 
 function App() {
   return (
     <div>
-      
+      <Question content="What is your favourite food?"/>
     </div>
   );
 }
