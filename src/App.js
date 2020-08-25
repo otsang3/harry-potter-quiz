ad02ae0d8a9464f3fdbc66e5d482b2a39cc218c2
otsang3 <ohhtsang@gmail.com>
2020-08-25 15:30:30 +0100

    added initial code to App.js, added quizQuestions.js api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Question from './components/Question';
 
 function App() {
+
+  const initialState = {
+    counter: 0,
+    questionId: 1,
+    question: '',
+    answerOptions: [],
+    answer: '',
+    answersCount: {},
+    result: ''
+  }
+
+  const [state, setState] = useState(initialState)
   return (
     <div>
       <Question content="What is your favourite food?"/>
