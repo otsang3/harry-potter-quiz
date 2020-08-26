4fd9eff01912dbfc39b5dba9e0cf12273cdb8ce7
otsang3 <ohhtsang@gmail.com>
2020-08-26 13:45:40 +0100

    amened quiz questions

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,86 +1,169 @@
 let quizQuestions = [
     {
-        question: "What franchise would you rather play a game from?",
+        question: "During the end-of-year exams, you notice that one of your classmates was using an enchanted quill. You come top of the class anyway, but they are second. What do you do?",
         answers: [
             {
-                type: "Microsoft",
-                content: "Halo"
+                type: "Gryffindor",
+                content: "Tell the professor immediately – cheating is wrong, no matter what."
             },
             {
-                type: "Nintendo",
-                content: "Pokemon"
+                type: "Ravenclaw",
+                content: "Nothing, but if I hadn't come top of the class, I'd definitely tell the professor."
             },
             {
-                type: "Sony",
-                content: "Uncharted"
+                type: "Hufflepuff",
+                content: "Encourage the other student to admit what they'd done to the professor."
+            },
+            {
+                type: "Slytherin",
+                content: "Give them a high five for managing to sneak the quill into the exam."
             }
         ]
     },
     {
-        question: "Which console would you prefer to play with friends?",
+        question: "You would be most hurt if a person called you...",
         answers: [
             {
-                type: "Microsoft",
-                content: "X-Box"
+                type: "Slytherin",
+                content: "Weak"
             },
             {
-                type: "Nintendo",
-                content: "Nintendo 64"
+                type: "Ravenclaw",
+                content: "Ignorant"
             },
             {
-                type: "Sony",
-                content: "Playstation 1"
+                type: "Hufflepuff",
+                content: "Unkind"
+            },
+            {
+                type: "Gryffindor",
+                content: "Boring"
             }
         ]
     },
     {
-        question: "Which of these racing franchises would you prefer to play a game from?",
+        question: "You're locked in a duel with a skilled opponent. They fire an unknown spell at you, and you shout…",
         answers: [
             {
-                type: "Microsoft",
-                content: "Forza"
+                type: "Gryffindor",
+                content: "Expelliarmus!"
             },
             {
-                type: "Nintendo",
-                content: "Mario Kart"
+                type: "Hufflepuff",
+                content: "Protego!"
             },
             {
-                type: "Sony",
-                content: "Gran Turismo"
+                type: "Ravenclaw",
+                content: "Stupefy!"
+            },
+            {
+                type: "Slytherin",
+                content: "Crucio!"
             }
         ]
     },
     {
-        question: "Which of these games do you think is best?",
+        question: "Which of your skills are you most proud of?",
         answers: [
             {
-                type: "Microsoft",
-                content: "BioShock"
+                type: "Ravenclaw",
+                content: "My ability to absorb new information."
             },
             {
-                type: "Nintendo",
-                content: "The Legend of Zelda: Ocarina of Time"
+                type: "Hufflepuff",
+                content: "My ability to make new friends."
             },
             {
-                type: "Sony",
-                content: "Final Fantasy VII"
+                type: "Slytherin",
+                content: "My ability to get what I want."
+            },
+            {
+                type: "Gryffindor",
+                content: "My ability to keep secrets."
             }
         ]
     },
     {
-        question: "What console would you prefer to own?",
+        question: "The first Quidditch match of the season is approaching, and you can't wait to get involved. What role are you playing?",
         answers: [
             {
-                type: "Microsoft",
-                content: "X-Box One"
+                type: "Ravenclaw",
+                content: "Seeker. I want the glory!"
             },
             {
-                type: "Nintendo",
-                content: "Wii U"
+                type: "Gryffindor",
+                content: "Chaser. I like to be involved, and work as part of the team."
             },
             {
-                type: "Sony",
-                content: "Playstation 4"
+                type: "Slytherin",
+                content: "Beater. I like having all that power."
+            },
+            {
+                type: "Hufflepuff",
+                content: "I'll be in the crowd, making sure supporter morale is high!"
+            }
+        ]
+    },
+    {
+        question: "It's Saturday, you've finished your homework, and you have some free time. You decide to spend some time away from your common room. Where do you go?",
+        answers: [
+            {
+                type: "Slytherin",
+                content: "The Forbidden Forest"
+            },
+            {
+                type: "Ravenclaw",
+                content: "The library"
+            },
+            {
+                type: "Hufflepuff",
+                content: "The kitchens"
+            },
+            {
+                type: "Gryffindor",
+                content: "The Room of Requirement"
+            }
+        ]
+    },
+    {
+        question: "What would you see in the Mirror of Erised?",
+        answers: [
+            {
+                type: "Slytherin",
+                content: "Myself, surrounded by riches"
+            },
+            {
+                type: "Hufflepuff",
+                content: "Myself, surrounded by my loving family and friends"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Myself, knowledgable above all"
+            },
+            {
+                type: "Gryffindor",
+                content: "Myself, experiencing a marvellous adventure"
+            }
+        ]
+    },
+    {
+        question: "Which animal would be your mascot?",
+        answers: [
+            {
+                type: "Slytherin",
+                content: "Serpent"
+            },
+            {
+                type: "Hufflepuff",
+                content: "Badger"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Eagle"
+            },
+            {
+                type: "Gryffindor",
+                content: "Lion"
             }
         ]
     }
